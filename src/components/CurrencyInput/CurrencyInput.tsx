9d1f21b37e6103363a564dcc32328744ed3d4fe9
Bruno Xavier <69484045+Sleepyblue@users.noreply.github.com>
2024-08-16 13:12:15 +0100

    Better handling the input currency formatting

--- src/components/CurrencyInput/CurrencyInput.tsx
+++ src/components/CurrencyInput/CurrencyInput.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./CurrencyInput.module.css";
 
 export interface CurrencyInputProps {
@@ -10,12 +10,33 @@ const CurrencyInput: React.FC<CurrencyInputProps> = ({
   userInput,
   onUserInputChange,
 }) => {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleBlur = () => {
+    setIsFocused(false);
+  };
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const displayValue = isFocused
+    ? userInput
+    : new Intl.NumberFormat(undefined, {
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 8,
+      }).format(Number(userInput));
+
   return (
     <input
       className={styles.currencyInput}
       type="text"
-      value={userInput}
+      min="0.01"
+      value={displayValue}
       onChange={(event) => onUserInputChange(event.target.value)}
+      onBlur={handleBlur}
+      onFocus={handleFocus}
+      placeholder="0.00"
     />
   );
 };
